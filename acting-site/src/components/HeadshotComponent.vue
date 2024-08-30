5e6c695bbc6a8d5ec729ec297f4cdd9e9a4b34a1
blakephillipsss <blake.kingfield@gmail.com>
2024-08-30 10:03:58 -0400

    Breaking Up Into Components

---
+++ acting-site/src/components/HeadshotComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="profile">
+      <h1>Blake H. Phillips</h1>
+      <p>(he/him/his)</p>
+      <img src="../assets/Blake H. Phillips Headshot.jpg" alt="" class="">
+      <p>Non-Union Toronto Actor</p>
+      <a href="mailto:acting@blakehphillips.com">acting@blakehphillips.com</a>
+      <p>+1 (416) 818-1156</p>
+    </div>
+  </template>
+  
+  <style scoped>
+    /* Profile Styles */
+    .profile h1 {
+      color: #f2f4ff;
+      font-family: "Bodoni MT Condensed", serif;
+      font-size: 2.8rem;
+    }
+  
+    .profile img {
+      height: auto;
+      max-width: 100%;
+    }
+  
+    .profile p, .profile a {
+      color: #f2f4ff;
+    }
+  
+    .profile p {
+      font-size: 1.2rem;
+    }
+  
+    .profile a {
+      text-decoration: underline;
+    }
+  </style>
---
+++ acting-site/src/components/NavComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="button-container">
+      <div class="demos-button">
+        <a href="#demo-section" @click.prevent="scrollTo('demo-section')">Demo Footage and Media</a>
+      </div>
+      <div class="demos-button">
+        <a href="#resume-section" @click.prevent="scrollTo('resume-section')">Resume</a>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+    /* Button Styles */
+    .button-container {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
+  
+    .demos-button {
+      background-color: #595758;
+      margin: 2%;
+      padding: 12px;
+      border-radius: 15px;
+      width: 100%;
+      transition: background-color 0.3s ease;
+    }
+  
+    .demos-button:hover {
+      background-color: #7a7879;
+    }
+  
+    .demos-button a {
+      text-decoration: none;
+      color: #f2f4ff;
+    }
+  </style>
